4a9696b3e8ce03d1fe85a80af4623f0c4151dae5
LoLaBurrbosa <lola33barros@gmail.com>
2021-11-22 14:36:58 -0300

    produtos e etc...

---
+++ assets/js/produtos.js
@@ -0,0 +1,12 @@
+var addBtn = document.querySelector("#addbottom a");
+var popup = document.querySelector("#popupbackground");
+var closeBtn = document.querySelector("#popup form a");
+addBtn.addEventListener("click", ()=>{
+    popup.style.display = "flex";
+    popup.style.transition = "display 5s";
+})
+
+closeBtn.addEventListener("click", ()=>{
+    popup.style.display = "none";
+    popup.style.transition = "display 5s";
+})
